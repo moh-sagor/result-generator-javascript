65579e02b4c93c78b63eb035ece9a35642570dbe
Sagor Hossain <sagorhassain4@gmail.com>
2023-02-01 23:14:58 +0600

    age calculator

--- script.js
+++ script.js
@@ -54,3 +54,35 @@ function get_result() {
     document.getElementById("modal_result").innerHTML = "You got GPA : " + gpa;
 }
 get_result();
+
+
+
+function calculate_age() {
+    var d = document.getElementById('date').value;
+    var m = document.getElementById('month').value;
+    var y = document.getElementById('year').value;
+
+    var date = new Date();
+    var d1 = date.getDate();
+    var m1 = 1 + date.getMonth();
+    var y1 = date.getFullYear();
+
+    var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if (d > d1) {
+        d1 = d1 + month[m1 - 1];
+        m1 = m1 - 1;
+    }
+    if (m > m1) {
+        m1 = m1 + 12
+        y1 = y1 - 1;
+    }
+
+    const finalDate = d1 - d;
+    const finalMonth = m1 - m;
+    const finalYear = y1 - y;
+
+
+    document.getElementById('display_age').innerHTML = "Your Age is " + finalYear + " Year " + finalMonth + " Month "
+        + finalDate + " Days"
+}
+
